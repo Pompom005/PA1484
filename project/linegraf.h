dd1fc425789406d2ad96bf3ae80f79560f0f4002
laura dushime <dshmlaura@gmail.com>
2025-11-19 14:38:55 +0100

    linegrafcommit

--- project/linegraf.h
+++ project/linegraf.h
@@ -4,16 +4,26 @@
 #include <lvgl.h>
 #include <LilyGo_AMOLED.h>
 #include <LV_Helper.h>
+#include <vector>
+#include<string>
+#include<algorithm>
 // I need to include the file of the graf
 using namespace std;
 
 class Linegraf{
     private:
-        int percentage;
         lv_obj_t * obj1; 
-        lv_obj_t * chart;
+        lv_obj_t * serie1;
+        vector<float> valuesforthegraf;
+        string location;
+        float mx =0;
+        float mn=0;
         // i need a pointer to the graf too 
     public:
-        Linegraf(lv_obj_t* parent, int num);
-        void percentfunction();
+        Linegraf(lv_obj_t* parent, const vector<float>& chosen, string name);
+        void replacedata(float max_value, float value_min, int numberofdots);
+        void adddatatolist(float data);
+        int getminmaxsize(const vector<float>& grafvalues);
+        void changelocation(string changed);
+        void thehelpfunction();
 };
