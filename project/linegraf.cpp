5ba3d0cfb2570d02a0118d1069987c186ebda0e5
laura dushime <dshmlaura@gmail.com>
2025-11-19 14:36:07 +0100

    Merge branch 'main' of https://github.com/Pompom005/PA1484

--- project/linegraf.cpp
+++ project/linegraf.cpp
@@ -1,19 +1,51 @@
-#include<linegraf.h>
-Linegraf:: Linegraf(lv_obj_t*parent, int num){
+#include<Linegraf.h>
+Linegraf:: Linegraf(lv_obj_t*parent, const vector<float>& chosen, string name){ // test chosen
     obj1 = lv_chart_create(parent);
-    lv_obj_set_size(obj1, 100, 100);
+    lv_obj_set_size(obj1, 500, 400);
     lv_obj_center(obj1);
+    lv_chart_set_type(obj1, LV_CHART_TYPE_LINE);
+    changelocation(name);
+    //first fill in the list
+    // getminmaxsize
+    int result = getminmaxsize(chosen);
+    // replacedata
+    replacedata(mx, mn, result);
+}
+void Linegraf:: replacedata(float max_value, float min_value, int numberofdots){
+    lv_chart_series_t* serie = lv_chart_add_series(obj1, lv_palette_main(LV_PALETTE_ORANGE), LV_CHART_AXIS_SECONDARY_Y);
+    uint32_t i;
+    for(i =0; i< numberofdots; i++){
+        lv_chart_set_next_value(obj1, serie, (int32_t)lv_rand(min_value, max_value));
 
-    chart = lv_chart_create(obj1);
-    lv_obj_set_width(chart, lv_pct(100));
-    lv_obj_set_flex_grow(chart, 1);
-    lv_chart_set_type(chart, LV_CHART_TYPE_BAR);
-    lv_chart_set_range(chart, LV_CHART_AXIS_PRIMARY_Y,0, 50);
-    lv_chart_set_range(chart, LV_CHART_AXIS_SECONDARY_Y,0, 100);
-    lv_chart_set_point_count(chart, 12);
-    lv_obj_set_style_radius(chart, 0, 0);
-    // create scale
+    }
+    lv_chart_refresh(obj1);
+}
 
+void Linegraf:: adddatatolist(float data){
+    valuesforthegraf.push_back(data);
 
 }
-void Linegraf:: percentfunction(){}
+
+int Linegraf:: getminmaxsize(const vector<float>& grafvalues){
+    if (grafvalues.empty()){
+        Serial.println("Empty");
+        return 0;
+    }
+    auto largest = max_element(grafvalues.begin(), grafvalues.end());
+    auto smallest = min_element(grafvalues.begin(), grafvalues.end());
+    mx = *largest;
+    mn = *smallest;
+    return grafvalues.size();
+}
+
+
+void Linegraf:: changelocation(string changed){
+    location = changed;
+    // either create a helpfunction or (adddatatolist directly?)
+    //adddatatolist();
+}
+
+void Linegraf:: thehelpfunction(){
+    //adddatatolist()
+}
+
